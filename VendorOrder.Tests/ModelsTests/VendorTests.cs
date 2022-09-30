28db19fd25cdb635b77b0c9e6996cfaac6758e1d
tgold83 <ted.jonathan.gold@gmail.com>
2022-09-30 09:38:53 -0700

    add first test passing for VendorTests

---
+++ VendorOrder.Tests/ModelsTests/VendorTests.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VendorOrder.Models;
+using System.Collections.Generic;
+using System;
+
+namespace VendorOrder.Tests
+{
+  [TestClass]
+  public class VendorTests 
+  {
+
+    // public void Dispose()
+    // {
+    //   Vendor.ClearAll();
+    // }
+
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("Michael's", "Pizza Parlor");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+  }
+}
--- VendorOrder/Models/Vendor.cs
+++ VendorOrder/Models/Vendor.cs
@@ -1,40 +1,42 @@
 using System.Collections.Generic;
 
-namespace ToDoList.Models
+namespace VendorOrder.Models
 {
-  public class Category
+  public class Vendor
   {
-    private static List<Category> _instances = new List<Category> {};
+    private static List<Vendor> _instances = new List<Vendor> {};
     public string Name { get; set; }
-    public int Id { get; }
-    public List<Item> Items { get; set; }
+    public string Description { get; set; }
+    // public int Id { get; }
+    // public List<Item> Items { get; set; }
 
-    public Category(string categoryName)
+    public Vendor(string vendorName, string description)
     {
-      Name = categoryName;
+      Name = vendorName;
+      Description = description;
       _instances.Add(this);
-      Id = _instances.Count;
-      Items = new List<Item>{};
+      // Id = _instances.Count;
+      // Items = new List<Item>{};
     }
 
-    public static void ClearAll()
-    {
-      _instances.Clear();
-    }
+//     public static void ClearAll()
+//     {
+//       _instances.Clear();
+//     }
 
-    public static List<Category> GetAll()
-    {
-      return _instances;
-    }
+//     public static List<Category> GetAll()
+//     {
+//       return _instances;
+//     }
 
-    public static Category Find(int searchId)
-    {
-      return _instances[searchId-1];
-    }
+//     public static Category Find(int searchId)
+//     {
+//       return _instances[searchId-1];
+//     }
 
-    public void AddItem(Item item)
-    {
-      Items.Add(item);
-    }
+//     public void AddItem(Item item)
+//     {
+//       Items.Add(item);
+//     }
   }
 }
