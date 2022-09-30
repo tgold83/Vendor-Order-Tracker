85eb910a94c274951a9e52a71168ad43d265cca7
tgold83 <ted.jonathan.gold@gmail.com>
2022-09-30 09:42:34 -0700

    add second test passing for VendorTests

--- VendorOrder.Tests/ModelsTests/VendorTests.cs
+++ VendorOrder.Tests/ModelsTests/VendorTests.cs
@@ -20,5 +20,20 @@ namespace VendorOrder.Tests
       Vendor newVendor = new Vendor("Michael's", "Pizza Parlor");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
+
+    [TestMethod]
+    public void GetName_ReturnsName_String()
+    {
+      //Arrange
+      string name = "Test Vendor";
+      string description = "Test Description";
+      Vendor newVendor = new Vendor(name, description);
+
+      //Act
+      string result = newVendor.Name;
+
+      //Assert
+      Assert.AreEqual(name, result);
+    }
   }
 }
